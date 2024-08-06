10ff23485ff21c39d0b5a3af696c1951251aa8a5
ManashSwain <manash0802@gmail.com>
2024-08-07 00:09:07 +0530

    made modifications

---
+++ app.js
@@ -0,0 +1,37 @@
+import express from "express";
+import cors from "cors";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const port = 3000;
+const app = express();
+const server = createServer(app);
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+  methods: ["GET", "POST"],
+};
+const io = new Server(server, {
+  cors: corsOptions,
+});
+
+// Middlewares
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("This is our Backend of chat Application");
+});
+
+io.on("connection", (socket) => {
+  console.log(`User connected with : ${socket.id}`);
+  socket.on("message", ({ data, roomID }) => {
+    io.to(roomID).emit("receive-message", data);
+  });
+
+  socket.on("room-details", (roomName) => {
+    socket.join(roomName);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
---
+++ readme.md
@@ -0,0 +1,16 @@
+Setting Up socket.io in backend 
+
+1. Initialize express App
+   express.js documentation : https://expressjs.com/ 
+
+2. Import createServer from http.
+
+3. Import Server from socket.io.
+
+4. Create a server variable and call the createServer method and pass app variable (from express) as argument.
+
+5. Create new instance of Server (from socket.io) and pass server as argument and store this in a variable io.
+
+6. You can pass corsOptions as 2nd argument.
+
+7. 
